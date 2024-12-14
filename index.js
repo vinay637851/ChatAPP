462cf41c07f8545519a1ea738172d76db981696c
vinaysharma <vs864606@gmail.com>
2024-12-14 23:25:16 +0530

    some new features added

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ let path=require('path');
 let http=require('http');
 let server=http.createServer(app);
 let io=require('socket.io')(server);
-
-
+let UserData="";
+let arr=[];
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"/public")));
 app.use(express.json());
@@ -14,16 +14,21 @@ app.set("views",path.join(__dirname,"/views"));
 
 io.on('connect',function(User){
     let id=User.id;
-    console.log("User connected",User.id);
+    let Detail={
+        "id":id,
+        "username":UserData
+    }
+    UserData="";
+    arr.push(Detail);
     User.on("send-message",function(chat){
-        io.emit('received-message',chat,id);
+        io.emit('received-message',chat,id,arr);
     })
     User.on("received-message",function(data){
         let chat="";
-        io.emit('received-message',chat,id);
+        io.emit('received-message',chat,id,arr);
     })
     User.on('update-typing',function(data){
-        io.emit('update-typing',id);
+        io.emit('update-typing',id,arr);
     })
 })
 
@@ -33,5 +38,13 @@ server.listen(3000,function(){
 })
 
 app.get("/",function(req,res){
-    res.render("chat.ejs");
+    res.render("home.ejs");
+})
+app.get("/chat",function(req,res){
+    res.render("chat.ejs",{username:UserData});
+})
+app.post("/user_details",function(req,res){
+    let {username,password}=req.body;
+    UserData=username;
+    res.redirect("/chat");
 })
--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ document.getElementById("data").addEventListener("keyup",function(e){
     if(e.key!='Enter'&&data.length>0){
         socket.emit('received-message',"hello");
     }
-    if(data.length==0&&e.key!='Enter'){
+    else if(data.length==0&&e.key!='Enter'){
         socket.emit('update-typing',"world");
     }
 })
@@ -16,7 +16,7 @@ document.querySelector('form').addEventListener("submit",function(e){
         socket.emit("send-message",data)
         let div=document.createElement('div');
         let span=document.createElement('span');
-        span.innerHTML = socket.id;
+        span.innerHTML = username;
         span.classList.add('span')
         div.classList.add('sender');
         let p=document.createElement('p');
@@ -31,20 +31,27 @@ document.querySelector('form').addEventListener("submit",function(e){
 })
 let prevId=null;
 let child=-1;
-socket.on('received-message',function(chat,id){
+socket.on('received-message',function(chat,id,arr){
     if(chat.length>0&&socket.id!=id){
-        ChatActivity(chat,id);
+        ChatActivity(chat,id,arr);
         prevId=null;
     }
     if(chat.length==0&&socket.id!=id&&prevId!=id){
-        ChatActivity("Typing...",id)
+        ChatActivity("Typing...",id,arr)
         prevId=id;
     }
 });
 
-socket.on("update-typing",function(id){
+socket.on("update-typing",function(id,arr){
+    let name="";
+    for(let i=0;i<arr.length;i++){
+        if(arr[i].id==id){
+            name=arr[i].username;
+            break;
+        }
+    }
     for(let i=container.children.length-1;i>=0;i--){
-        if(container.children[i].classList.contains('recieved')&&container.children[i].children[0].innerHTML==id){
+        if(container.children[i].classList.contains('recieved')&&container.children[i].children[0].innerHTML==name){
             if(container.children[i].children[1].innerHTML=="Typing..."){
                 container.removeChild(container.children[i]);
                 prevId=null;
@@ -54,10 +61,17 @@ socket.on("update-typing",function(id){
     }
 })
 
-function ChatActivity(chat,id){
+function ChatActivity(chat,id,arr){
     let div=document.createElement('div');
+    let name="";
+    for(let i=0;i<arr.length;i++){
+        if(arr[i].id==id){
+            name=arr[i].username;
+            break;
+        }
+    }
     for(let i=container.children.length-1;i>=0;i--){
-        if(container.children[i].classList.contains('recieved')&&container.children[i].children[0].innerHTML==id){
+        if(container.children[i].classList.contains('recieved')&&container.children[i].children[0].innerHTML==name){
             if(container.children[i].children[1].innerHTML=="Typing..."){
                 container.removeChild(container.children[i]);
                 break;
@@ -65,7 +79,7 @@ function ChatActivity(chat,id){
         }
     }
     let span=document.createElement('span');
-    span.innerHTML = id;
+    span.innerHTML = name;
     span.classList.add('span2')
     div.classList.add('recieved')
     let p=document.createElement('p');
